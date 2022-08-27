afc16614eb8b90c75c6cd74d0ebd4ed91da90ec3
Bobby <s215818@dtu.dk>
2022-08-27 19:52:45 +0200

    Refactor

--- Src/commandlist.py
+++ Src/commandlist.py
@@ -4,34 +4,55 @@ class CommandList:
 
         self.command_list = {
             # Browsers
-            "Open Firefox": ["firefox"],
-            "Open Chrome": ["google-chrome"],
-            "Open Opera": ["opera"],
-            "Open Bing": ["microsoft-edge"],
-            "Open Edge": ["microsoft-edge"],
+            "Browsers": {
+                "Open Firefox": ["firefox"],
+                "Open Chrome": ["google-chrome"],
+                "Open Opera": ["opera"],
+                "Open Bing": ["microsoft-edge"],
+                "Open Edge": ["microsoft-edge"],
+                "Open Brave": ["brave-browser"],
+                "Open Tor": ["tor"],
+                "Open DuckDuckGo": ["duckduckgo"],
+                "Open Chromium": ["chromium"],
+            },
 
             # Miscellaneous websites
-            "Open GitHub": ["www.github.com"],
-            "Open YouTube": ["www.youtube.com"],
-            "Open Twitch": ["www.twitch.tv"],
-            "Open Netflix": ["www.netflix.com"],
-            "Open LinkedIn": ["www.linkedin.com"],
-            "Open ChatGPT": ["beta.openai.com"],
+            "Websites": {
+                "Open GitHub": ["www.github.com"],
+                "Open YouTube": ["www.youtube.com"],
+                "Open Twitch": ["www.twitch.tv"],
+                "Open Netflix": ["www.netflix.com"],
+                "Open Amazon": ["www.amazon.com"],
+                "Open Facebook": ["www.facebook.com"],
+                "Open Instagram": ["www.instagram.com"],
+                "Open Twitter": ["www.twitter.com"],
+                "Open Reddit": ["www.reddit.com"],
+                "Open Wikipedia": ["www.wikipedia.org"],
+                "Open Google": ["www.google.com"],
+                "Open Google Maps": ["www.google.com/maps"],
+                "Open Google Translate": ["translate.google.com"],
+                "Open LinkedIn": ["www.linkedin.com"],
+                "Open ChatGPT": ["beta.openai.com"],
+            },
 
             # Miscellaneous programs
-            "Open Spotify": ["spotify"],
-            "Open Discord": ["discord"],
-            "Open Steam": ["steam"],
-            "Open PyCharm": ["pycharm"],
-            "Open Clion": ["clion"],
-            "Open IntelliJ": ["intellij"],
-            "Open VS Code": ["code"],
-            "Open Files": ["nautilus"],
+            "Programs": {
+                "Open Spotify": ["spotify"],
+                "Open Discord": ["discord"],
+                "Open Steam": ["steam"],
+                "Open PyCharm": ["pycharm"],
+                "Open Clion": ["clion"],
+                "Open IntelliJ": ["intellij"],
+                "Open VS Code": ["code"],
+                "Open Files": ["nautilus"],
+            },
 
             # University websites
-            "Open DTU Learn": ["learn.inside.dtu.dk"],
-            "Open DTU Inside": ["inside.dtu.dk"],
-            "Open Google Drive": ["drive.google.com"],
-            "Open Gmail": ["mail.google.com"],
-            "Open Outlook": ["outlook.office.com"],
+            "University Websites": {
+                "Open DTU Learn": ["learn.inside.dtu.dk"],
+                "Open DTU Inside": ["inside.dtu.dk"],
+                "Open Google Drive": ["drive.google.com"],
+                "Open Gmail": ["mail.google.com"],
+                "Open Outlook": ["outlook.office.com"],
+            }
         }
