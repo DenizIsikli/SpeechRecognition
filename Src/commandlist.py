789294330214f3276aca0bff1494ddf1be49d9b0
Bobby <s215818@dtu.dk>
2022-09-11 03:48:27 +0200

    Command list

---
+++ Src/commandlist.py
@@ -0,0 +1,35 @@
+class CommandList:
+    def __init__(self):
+        self.command_list = {
+            # Browsers
+            "Open Firefox": ["firefox"],
+            "Open Chrome": ["google-chrome"],
+            "Open Opera": ["opera"],
+            "Open Bing": ["microsoft-edge"],
+            "Open Edge": ["microsoft-edge"],
+
+            # Miscellaneous websites
+            "Open GitHub": ["www.github.com"],
+            "Open YouTube": ["www.youtube.com"],
+            "Open Twitch": ["www.twitch.tv"],
+            "Open Netflix": ["www.netflix.com"],
+            "Open LinkedIn": ["www.linkedin.com"],
+            "Open ChatGPT": ["beta.openai.com"],
+
+            # Miscellaneous programs
+            "Open Spotify": ["spotify"],
+            "Open Discord": ["discord"],
+            "Open Steam": ["steam"],
+            "Open PyCharm": ["pycharm"],
+            "Open Clion": ["clion"],
+            "Open IntelliJ": ["intellij"],
+            "Open VS Code": ["code"],
+            "Open Files": ["nautilus"],
+
+            # University websites
+            "Open DTU Learn": ["learn.inside.dtu.dk"],
+            "Open DTU Inside": ["inside.dtu.dk"],
+            "Open Google Drive": ["drive.google.com"],
+            "Open Gmail": ["mail.google.com"],
+            "Open Outlook": ["outlook.office.com"],
+        }
