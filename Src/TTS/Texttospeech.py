d9ead7eb73a0ac6f01a56d24ca29262ed692a8fb
Bobby <s215818@dtu.dk>
2023-10-19 18:54:33 +0200

    Fix

--- Src/TTS/Texttospeech.py
+++ Src/TTS/Texttospeech.py
@@ -6,22 +6,15 @@ class TextToSpeech:
     def __init__(self):
         self.tts_init = pyttsx3.init()
 
-    def tts(self, text):
+    def tts(self, text, voice: str = "0"):
         engine = self.tts_init
+        voices = engine.getProperty('voices')
+        engine.setProperty('voice', voices[int(voice)].id)
         engine.say(text)
         engine.runAndWait()
         time.sleep(2)
 
-    @staticmethod
-    def list_voices_from_registry(voice: int = 0):
-        engine = pyttsx3.init()
-        voices = engine.getProperty('voices')
-
-        engine.setProperty('voice', voices[voice].id)
-
-        engine.runAndWait()
-
 
 if __name__ == "__main__":
     tts = TextToSpeech()
-    tts.list_voices_from_registry(voice=0)
+    tts.tts("Dennis was made of poop and brain farts", voice="0")
