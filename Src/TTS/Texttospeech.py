2a0b7abaf012581afbf51da1cde5ea7c54662d65
Bobby <s215818@dtu.dk>
2023-10-19 17:48:47 +0200

    Voice options

--- Src/TTS/Texttospeech.py
+++ Src/TTS/Texttospeech.py
@@ -1,11 +1,27 @@
-import pyttsx3
 import time
+import pyttsx3
 
 
 class TextToSpeech:
-    @staticmethod
-    def tts(text):
-        engine = pyttsx3.init()
+    def __init__(self):
+        self.tts_init = pyttsx3.init()
+
+    def tts(self, text):
+        engine = self.tts_init
         engine.say(text)
         engine.runAndWait()
         time.sleep(2)
+
+    @staticmethod
+    def list_voices_from_registry(voice: int = 0):
+        engine = pyttsx3.init()
+        voices = engine.getProperty('voices')
+
+        engine.setProperty('voice', voices[voice].id)
+
+        engine.runAndWait()
+
+
+if __name__ == "__main__":
+    tts = TextToSpeech()
+    tts.list_voices_from_registry(voice=0)
