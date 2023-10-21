9b7efbb2b06b7d53d7c01f93dab2373ac93061d2
Bobby <s215818@dtu.dk>
2023-10-21 21:13:08 +0200

    thread

--- Src/main.py
+++ Src/main.py
@@ -1,9 +1,14 @@
 from Tkinter import Tkinter as Tk
 from Listener import Listener as Ls
+import threading
 
 if __name__ == "__main__":
     tk = Tk.TkinterWindow()
-    ls = Ls.Listener()
+    ls = Ls.Listener(tk)
+
+    # Create a thread to run the listener
+    listener_thread = threading.Thread(target=ls.listen)
+
+    listener_thread.start()
 
     tk.run()
-    ls.listen()
